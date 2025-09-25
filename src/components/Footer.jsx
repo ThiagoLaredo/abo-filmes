9092223c69d3237b2ee6013909a5429462ce4d7c
ThiagoLaredo <thiago@olatu.com.br>
2025-09-25 10:22:21 -0300

    mensagem

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,35 @@
 // src/components/Footer.jsx
-import React from "react";
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./Footer.css";
 
 export default function Footer() {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.fromTo(
+      ".footer p",
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: ".footer",
+          start: "top 90%", // começa quando o footer estiver 80% visível
+          toggleActions: "play none none none",
+        },
+      }
+    );
+  }, []);
+
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} Abó Filmes — Todos os direitos reservados.</p>
+      <p>
+        © {new Date().getFullYear()} Abó Filmes — Todos os direitos reservados.
+      </p>
     </footer>
   );
 }
